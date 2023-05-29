a8a21a1e1137ccf1e8a8e9d364a72c47bdcac065
rakib54 <rakibur5454@gmail.com>
2023-05-29 14:00:28 +0600

    set up django project and forms

---
+++ first_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'first_app/home.html')
+
+
+def about(request):
+    return render(request, 'first_app/about.html')
+
+
+def form(request):
+    return render(request, 'first_app/form.html')
