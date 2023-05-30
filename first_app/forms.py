7d413e579677447e241fadfafbd5874ca6f0cb12
rakib54 <rakibur5454@gmail.com>
2023-05-30 11:16:21 +0600

    Exploring different fields in django forms

--- first_app/forms.py
+++ first_app/forms.py
@@ -3,4 +3,11 @@ from django import forms
 
 class contactForm(forms.Form):
     name = forms.CharField(label="Username")
-    email = forms.EmailField(label="Username")
+    email = forms.EmailField(label="Email")
+    age = forms.IntegerField()
+    weight = forms.FloatField(label="Weight")
+    balance = forms.DecimalField()
+    CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    size = forms.ChoiceField(choices=CHOICES)
+    MEAL = [('P', 'Pepperoni'), ('C', 'Cheese'), ('B', 'Beef')]
+    pizza = forms.MultipleChoiceField(choices=MEAL)
